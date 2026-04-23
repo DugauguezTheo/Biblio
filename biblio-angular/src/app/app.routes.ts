09e138fb945f82a066ce7c0c700ff91d1196b0c5
Jean-Guy WINTREBERT <jg.wintrebert@orange.fr>
2026-04-23 12:15:53 +0200

    Added Avis to route

--- biblio-angular/src/app/app.routes.ts
+++ biblio-angular/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AccueilPage } from './page/accueil-page/accueil-page';
 import { CollectionsPage } from './page/collections-page/collections-page';
 import { Livres } from './page/livres/livres';
-import { authGuard } from './guard/auth-guard';
+// import { authGuard } from './guard/auth-guard';
 import { Editeurs } from './page/editeurs/editeurs';
 import { AuteurPage } from './page/auteur-page/auteur';
 import { AvisPage } from './page/avis-page/avis-page';
@@ -12,12 +12,20 @@ import { AvisPage } from './page/avis-page/avis-page';
 
 export const routes: Routes = [
 
-    { path: 'home', component: AccueilPage, canActivate: [authGuard] },
-    { path: 'livre', component: Livres, canActivate: [authGuard] },
-    { path: 'auteur', component: AuteurPage, canActivate: [authGuard] },
-    { path: 'collection', component: CollectionsPage, canActivate: [authGuard] },
-    { path: 'editeur', component: Editeurs, canActivate: [authGuard] },
-    { path: 'avis', component: AvisPage, canActivate: [authGuard] },
+    // { path: 'home', component: AccueilPage, canActivate: [authGuard] },
+    // { path: 'livre', component: Livres, canActivate: [authGuard] },
+    // { path: 'auteur', component: AuteurPage, canActivate: [authGuard] },
+    // { path: 'collection', component: CollectionsPage, canActivate: [authGuard] },
+    // { path: 'editeur', component: Editeurs, canActivate: [authGuard] },
+    // { path: 'avis', component: AvisPage, canActivate: [authGuard] },
+
+    
+    { path: 'home', component: AccueilPage },
+    { path: 'livre', component: Livres },
+    { path: 'auteur', component: AuteurPage },
+    { path: 'collection', component: CollectionsPage },
+    { path: 'editeur', component: Editeurs },
+    { path: 'avis', component: AvisPage },
 
     //{ path: 'login', component: LoginPage },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
