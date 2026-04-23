3e2e401c09577cab0ca90bd6cb246557a065e67b
Matth-Cbn <matthieu.corbin@outlook.fr>
2026-04-23 11:26:23 +0200

    Auteur Angular (model, service, html)

---
+++ biblio-angular/src/app/page/auteur-page/auteur.ts
@@ -0,0 +1,74 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Observable, startWith, Subject, switchMap } from 'rxjs';
+import { Auteur } from '../../model/auteur';
+import { AuteurService } from '../../service/auteur-service';
+
+@Component({
+  selector: 'app-auteur',
+  imports: [ CommonModule, ReactiveFormsModule ],
+  templateUrl: './auteur.html',
+  styleUrl: './auteur.css',
+})
+export class AuteurPage {
+  private titleService: Title = inject(Title);
+  private auteurService: AuteurService = inject(AuteurService);
+  private formBuilder: FormBuilder = inject(FormBuilder);
+
+  protected auteurs$!: Observable<Auteur[]>;
+  private refresh$: Subject<void> = new Subject<void>();
+
+  protected formAuteur!: FormGroup;
+  protected formNomCtrl!: FormControl;
+  protected formPrenomCtrl!: FormControl;
+  protected formNationaliteCtrl!: FormControl;
+
+  ngOnInit(): void {
+    this.titleService.setTitle("Liste des auteurs");
+
+    this.auteurs$ = this.refresh$.pipe(
+      startWith(0), // Initialisation => forcer le chargement une première fois
+      switchMap(() => this.auteurService.findAll()) // Transformer au moment du next()
+    );
+
+    // Fabrication du formulaire avec le FormBuilder
+    this.formNomCtrl = this.formBuilder.control("", Validators.required);
+    this.formPrenomCtrl = this.formBuilder.control("", Validators.required);
+    this.formNationaliteCtrl = this.formBuilder.control("", Validators.required);
+
+    this.formAuteur = this.formBuilder.group({
+      // Description des contrôles du formulaire
+      nom: this.formNomCtrl,
+      prenom: this.formPrenomCtrl,
+      nationalite: this.formNationaliteCtrl
+    });
+  }
+
+  private reload() {
+    this.refresh$.next();
+
+    // Sinon, rechargement de la page
+    // this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+    //   this.router.navigate([ '/matiere' ]);
+    // });
+  }
+
+  public addAuteur() {
+    const auteur: Auteur = {
+      id: 0,
+      nom: this.formNomCtrl.value,
+      prenom: this.formPrenomCtrl.value,
+      nationalite: this.formNationaliteCtrl.value,
+  };
+
+  this.auteurService.add(auteur).subscribe(() => this.reload());
+  }
+
+  public deleteAuteur(auteur: Auteur) {
+    this.auteurService.deleteById(auteur.id).subscribe(() => this.reload());
+  }
+}
+
+
