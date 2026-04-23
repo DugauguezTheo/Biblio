baffae16c208884001a8f921424a2239dbfe74b7
Tanguy NEMORIN <tanguy.nemorin@free.fr>
2026-04-23 13:47:41 +0200

    Ajout de la sécurité qui accepte tout le monde :)

---
+++ biblio/src/main/java/formation_sopra/biblio/security/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package formation_sopra.biblio.security.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorization -> {
+            authorization.requestMatchers("/**").permitAll();
+        });
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"));
+
+        http.cors(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    // Politique CORS : on autorise http://localhost:4200 uniquement
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+
+        corsSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return corsSource;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
