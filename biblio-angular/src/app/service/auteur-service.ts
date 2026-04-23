66ca61ced8e15386cc30a8554de3ccd8e2da51d8
Matth-Cbn <matthieu.corbin@outlook.fr>
2026-04-23 12:06:29 +0200

    CRUD Auteur Complété

--- biblio-angular/src/app/service/auteur-service.ts
+++ biblio-angular/src/app/service/auteur-service.ts
@@ -9,16 +9,24 @@ import { Auteur } from '../model/auteur';
 export class AuteurService {
     constructor(private http: HttpClient) { }
 
-  public findAll(): Observable<Auteur[]> {
+  public findAuteurById(id: number) {
+      return this.http.get<Auteur>(`/auteur/${id}`);
+    }
+
+  public findAllAuteur(): Observable<Auteur[]> {
     return this.http.get<Auteur[]>("/auteur");
   }
 
-  public add(auteur: Auteur): Observable<Auteur> {
+  public addAuteur(auteur: Auteur): Observable<Auteur> {
     return this.http.post<Auteur>("/auteur", auteur);
   }
 
-  public deleteById(id: number): Observable<void> {
+  public updateCollection(auteur: Auteur) {
+      return this.http.put(`/auteur/${auteur.id}`, auteur);
+    }
+
+  public deleteAuteurById(id: number): Observable<void> {
     return this.http.delete<void>(`/auteur/${ id }`);
   }
 }
-}
+
