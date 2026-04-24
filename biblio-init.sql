10687c70e1802119abf75b8b5fae64de442aadf3
Lauriane Bruna <brn.lauriane@gmail.com>
2026-04-24 09:48:32 +0200

    Modif docker-compose et biblio-init.sql pour la database

---
+++ biblio-init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS biblio;
+
+USE biblio;
+
+CREATE TABLE `utilisateur` (
+  `id` varchar(255) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+INSERT INTO utilisateur (id, username, password) VALUES ('jeremy', 'jeremy', '$2a$10$zFeTn0rQKrsMXIT2I2NAl.70YWXs05/XyJsnSsznDjB4C.T0yv8hC');
