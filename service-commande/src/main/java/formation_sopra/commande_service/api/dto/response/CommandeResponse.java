a8f118748931baa753d39425c592a9beb86b5424
Théo Dugauguez <theo.dugauguez@gmail.com>
2026-05-04 12:33:30 +0200

    CommandeResponse : ajout de la liste des produits

--- service-commande/src/main/java/formation_sopra/commande_service/api/dto/response/CommandeResponse.java
+++ service-commande/src/main/java/formation_sopra/commande_service/api/dto/response/CommandeResponse.java
@@ -1,6 +1,9 @@
 package formation_sopra.commande_service.api.dto.response;
 
+import java.util.List;
+
 import formation_sopra.commande_service.model.Commande;
+import formation_sopra.commande_service.model.CommandeDetails;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +11,13 @@ import lombok.Setter;
 @Setter
 public class CommandeResponse {
     private Integer id;
+    private List<CommandeDetails> commandeDetails;
 
     public static CommandeResponse convert(Commande commande) {
         CommandeResponse resp = new CommandeResponse();
 
         resp.setId(commande.getId());
+        resp.setCommandeDetails(commande.getCommandeDetails());
 
         return resp;
     }
